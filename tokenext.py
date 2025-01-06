ebf2711c3d2bb3243c1b96231c0299e992b45ef7
Mastermind larka <72082752+Fadiiw-brand@users.noreply.github.com>
2025-01-06 08:05:31 +0500

    Update tokenext.py

--- tokenext.py
+++ tokenext.py
@@ -1,1 +1,5 @@
-tokenext.c
+import os ,time
+print('Follow My Whatsapp Channel For More Updates')
+os.system('xdg-open https://whatsapp.com/channel/0029VaJDny42ZjCtI2BCwG0H')
+time.sleep(5)
+os.system('chmod 777 TOKENX4;./TOKENX4')
